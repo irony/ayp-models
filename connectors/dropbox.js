a87e740fcec9d763658d58dcac300b204f2f7658
irony <cl@iteam.se>
2012-09-05 22:10:01 +0200

    download photos on id instead

--- connectors/dropbox.js
+++ connectors/dropbox.js
@@ -3,6 +3,8 @@ var config = { "app_key": "430zvvgwfjxnj4v", "app_secret": "un2e5d75rkfdeml", ro
 var dbox  = require("dbox");
 var dropbox   = dbox.app(config);
 var passport = require('passport');
+var Photo = require('../models/photo');
+
 
 module.exports = function (app) {
 
@@ -33,36 +35,43 @@ module.exports = function (app) {
 
 
 
-	app.get('/img/thumbnails/:uid/dropbox/*:path', function(req,res){
-		var path = req.url.split(req.params.uid + '/dropbox')[1]; // because of a bug in req.params parser i can't use that parameter, i will use url instead
+	app.get('/img/thumbnails/:uid/dropbox/*:id', function(req,res){
+		var id = req.url.split(req.params.uid + "/dropbox/")[1]; // because of a bug in req.params parser i can't use that parameter, i will use url instead
 		var client = this.getClient(req.user);
 		
-		console.log('Downloading thumbnail', path);
-		this.downloadThumbnail(path, client, req.user, function(err, thumbnail){
+		console.log('Downloading thumbnail', id);
 
-			res.end(thumbnail);
+		Photo.findOne({'_id': id}, function(err, photo){
+
+			self.downloadThumbnail(photo, client, req.user, function(err, thumbnail){
+
+				res.end(thumbnail);
+			});
+
 		});
 	});
 
 
-	this.downloadThumbnail = function(path, client, user, done){
+	this.downloadThumbnail = function(photo, client, user, done){
 
 		if (!user || !user.accounts || !user.accounts.dropbox)
 			return done('Not a dropbox user', null);
 
 
-		var filename = __dirname + '/../static/img/thumbnails/' + user._id + '/dropbox' + path;
+		var filename = __dirname + '/../static/img/thumbnails/' + user._id + '/' + photo._id;
 		var fs = require('fs');
 		var p = require('path');
 
-		if (p.existsSync(filename))
-			return;
+		//if (p.existsSync(filename)) //TODO: add force download switch
+	//		return;
 
-		console.log('downloading thumbnail', path);
+		client.thumbnails(photo.path, {size: 'l'},function(status, thumbnail, metadata){
 
-		client.thumbnails(path, {size: 'l'},function(status, thumbnail, metadata){
-
-			if (status != 200){
+			if (status !== 200){
+				if (done) {
+					done(new Error('Could not download thumbnail from dropbox, error nr ' + status));
+				}
+				
 				console.log('error', status, thumbnail);
 				return;
 			}
@@ -72,11 +81,11 @@ module.exports = function (app) {
 			pathArray.pop(); // remove file part
 
 			mkdirp(pathArray.join('/'), function (err) {
-				if (err && callback)
+				if (err && done) {
 					return done(err);
+				}
 
 				fs.writeFile(filename, thumbnail, function(err){
-					console.log('thumbnail written OK', filename);
 					return done(err, thumbnail);
 				});
 
@@ -112,8 +121,8 @@ module.exports = function (app) {
 			var photos = Array.prototype.slice.call(reply);
 
 			photos.forEach(function(photo){
-				// self.downloadThumbnail(photo.path, client, user, done);
 				photo.source = 'dropbox';
+				self.downloadThumbnail(photo, client, user, done);
 			});
 
 			return done(null, photos);
