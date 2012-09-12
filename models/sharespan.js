9510ea13989041d5095fcde97de350756e794a2b
irony <cl@iteam.se>
2012-09-12 22:36:50 +0200

    Added sharespan

---
+++ models/sharespan.js
@@ -0,0 +1,14 @@
+
+
+var mongoose = require('mongoose'),
+  User = require('./user')(mongoose).Schema,
+    Schema = mongoose.Schema;
+
+var ShareSpanSchema = new mongoose.Schema({
+  added : { type: Date, default: Date.now()},
+  startDate: {type : Date},
+  stopDate: {type : Date},
+  members : { type: [User]}
+});
+
+module.exports = mongoose.model('ShareSpan', ShareSpanSchema);
