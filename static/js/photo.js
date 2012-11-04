72436d6a99adb6ac59c1b58d033dca3ffc10b40b
irony <cl@iteam.se>
2012-11-05 00:58:35 +0100

    added html viewer

--- static/js/photo.js
+++ static/js/photo.js
@@ -1,24 +1,42 @@
 function PhotoController($scope, $http){
   $scope.photos = [];
+  $scope.groups = {};
   $scope.dateRange = new Date();
 
   var counter = 0;
   $scope.loadMore = function() {
       console.log('loadMore', counter);
 
-    var query = {skip : counter};
+    var query = {skip : counter, limit: 400};
     $http.get('/photoFeed', query)
     .success(function(data){
-      
+
       (data||[]).forEach(function(photo){
         photo.src='/img/thumbnails/' + photo.source + '/' + photo._id;
+        photo.interesting = Math.random() * 100; // dummy value now. TODO: change to real one
+        photo.class = "i" + Math.round(photo.interesting / 20);
+        var group = $scope.getGroup(photo);
+        group.push(photo);
       });
-      
 
-
+      console.log($scope.groups);
       $scope.photos.push.apply($scope.photos, data);
       counter += data.length;
     });
+  };
+
+  $scope.getGroup = function(photo){
+    // group on date per default, TODO: add switch and control for this
+    var groupName = photo.taken.split('T')[0],
+        group = $scope.groups[groupName] = $scope.groups[groupName] || [];
+    
+    // split the groups if they are too big
+    while(group.length > 20) {
+      groupName = groupName + "_2";
+      group = $scope.groups[groupName] = $scope.groups[groupName] || [];
+    }
+
+    return group;
   };
 
   $scope.loadMore();
