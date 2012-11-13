d707883f1e7441cb299eae6149c087599f689eaf
irony <cl@iteam.se>
2012-11-13 19:42:12 +0100

    typography

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ var express = require('express')
 var MongoStore = require('connect-mongo')(express);
 var passport = require('./auth/passport');
 
-
-
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
 //   the request is authenticated (typically via a persistent login session),
