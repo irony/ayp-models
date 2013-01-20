5ccb92197cbb328f615ec55ac87c4d10f70a487f
irony <cl@iteam.se>
2013-01-21 00:26:27 +0100

    add rank

---
+++ jobs/updateRank.js
@@ -0,0 +1,89 @@
+var ObjectId = require('mongoose').Types.ObjectId,
+    timeago = require('timeago'),
+    Photo = require('../models/photo'),
+    async = require('async'),
+    emit = {}, // fool jsLint
+    mongoose = require('mongoose');
+
+
+
+module.exports = function(){
+
+  var map = function(){
+
+    var self = this;
+
+    for(var user in self.copies){
+      if (user && self.copies && self.copies[user]){
+
+        var copy = self.copies[user];
+
+        emit(user, {id: self._id, interestingness: copy.interestingness});
+
+        /*
+        1 : 1:100
+        2 : 1:50
+        3 : 1:0
+        4 : 1:250
+
+        1 : 2:175
+        2 : 2:20
+        3 : 2:5
+        4 : 2:150
+
+        => 1: [4,1,2,3], 2: [1,4,2,3]
+        */
+
+      }
+    }
+
+  };
+
+  var reduce = function(userId, photos){
+
+    var returnValue = 0;
+    var count = 0;
+    var result = photos.sort(function(a,b){
+      return b.interestingness - a.interestingness;
+    }).map(function(photo){
+      return photo.id;
+    });
+
+    return {toplist: result, count: photos.length};
+  };
+
+  console.log('Starting map/reduce ranking...');
+
+// add query to only reduce modified images
+  Photo.mapReduce({map:map, reduce:reduce, out : {replace : 'ranking'}, verbose: true}, function(err, model, stats){
+
+    console.log('Done with ranking.', stats);
+
+    if (err) throw err;
+
+    model.find(function(err, ranking){
+      ranking.forEach(function(photoRank){
+        var userId = photoRank._id;
+        var photos = photoRank.value.toplist;
+        var maxRank = photos.length;
+        var rank = 0;
+        
+        photos.forEach(function(photoId){
+          rank++;
+
+          var setter = {$set : {}};
+          var normalizedRank = ((maxRank - rank) / maxRank) * 100;
+          setter.$set['copies.' + userId + '.rank'] = normalizedRank;
+          setter.$set['copies.' + userId + '.top'] = rank;
+
+          Photo.update({_id : photoId}, setter, function(err, photo){
+            if (err) console.log('error when updating rank:', err);
+          });
+        });
+      });
+
+    });
+  });
+
+
+};
