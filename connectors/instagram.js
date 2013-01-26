d0b271f46e72d673d23500d5313db3b7b7100305
irony <cl@iteam.se>
2013-01-26 18:07:30 +0100

    moved structure to client and server

--- connectors/instagram.js
+++
@@ -1,8 +0,0 @@
-var passport = require('passport');
-var Connector = require('./connectorBase');
-
-module.exports = function (app) {
-
-
-};
-module.exports.prototype = Connector.prototype; //inherit from Connector base
--- routes/404.js
+++
@@ -1,9 +0,0 @@
-var ViewModel = require('./viewModel');
-
-module.exports = function(app){
-
-  app.get('/*', function(req, res){
-      res.render('404.ejs', new ViewModel(req.user));
-  });
-
-};
