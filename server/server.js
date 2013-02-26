0a34dffced414fd975927ec735214b53b07c067b
irony <cl@iteam.se>
2013-02-26 12:44:31 +0100

    added spdy

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 var app = require('./app').init();
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var spdy = require('spdy');
 
 require('./routes/connectors')(app);
 require('./routes/share')(app);
@@ -14,6 +18,15 @@ require('./utils/strings');
 /* The 404 Route (ALWAYS Keep this as the last route) */
 require('./routes/404')(app);
 
+
+var options = {
+    ca:   fs.readFileSync(__dirname + '/../ssl/sub.class1.server.ca.pem'),
+    key:  fs.readFileSync(__dirname + '/../ssl/ssl.key'),
+    cert: fs.readFileSync(__dirname + '/../ssl/ssl.crt')
+  };
+
+// attach express handler function to TWO servers, one for http and one for https
 app.listen(process.env.PORT || 3000);
+spdy.createServer(options, app.handle.bind(app)).listen(443);
 
 console.log("Listening on port %d in %s mode", app.address().port, app.settings.env);
