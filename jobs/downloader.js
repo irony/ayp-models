2529b2301c6f9a3b07554ed7183d7d363ab6f7e5
irony <cl@iteam.se>
2013-04-28 19:29:51 +0200

    actual saving of new photos 5 limit

--- jobs/downloader.js
+++ jobs/downloader.js
@@ -119,7 +119,7 @@ var downloader = {
     .where('store.original.stored').exists(false)
     .where('store.error').exists(false) // skip photos with previous download problems
     .sort('-taken') // todo: images before videos
-    .limit(20);
+    .limit(5);
 
     var downloadAllResults = function downloadAllResults(err, photos){
       // console.log('[50]Found %d photos without downloaded images. Downloading...', photos.length);
