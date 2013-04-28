e3d262b82c9a5660aa7a6450fc94a876173353e3
irony <cl@iteam.se>
2013-04-28 16:38:14 +0200

    added user

---
+++ server/api/user.js
@@ -0,0 +1,20 @@
+var User = require('../../models/user');
+var async = require('async');
+var ObjectId = require('mongoose').Types.ObjectId;
+var _ = require('underscore');
+
+module.exports = function(app){
+
+
+  app.get('/api/user/exist', function(req, res){
+
+    User.find({'emails.value':req.query.q}).count(function(err, result){
+
+      return res.json(result > 0);
+
+    });
+
+  });
+
+
+};
