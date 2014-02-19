378b654f4cfc2992fd906da88e7c68e77db17a4a
irony <cl@iteam.se>
2014-02-19 10:51:27 -0800

    added config search

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 var nconf = require('nconf');
 
+nconf
+  .env() // both use environment and file
+  .file({file: 'config.json', dir:'../', search: true});
+
+
 var models = module.exports = {
   group : require('./models/group'),
   photo : require('./models/photo'),
@@ -11,7 +16,16 @@ var models = module.exports = {
   passport : require('./auth/passport'),
   init : function(){
     try {
+      /*if (process.env.NODE_ENV !== 'production'){
+        mongoose.set('debug', true);
+      }*/
+
       mongoose.connect(nconf.get('mongoUrl'));
+      mongoose.connection.on('connected', function(){
+        console.log('db connected to ' + nconf.get('mongoUrl'));
+      });
+
+
       return models;
     } catch (err) {
       console.log(("Setting up failed to connect to " + nconf.get('mongoUrl')).red, err.message);
