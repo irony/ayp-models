8183a953f4463c2c91aa3670d8a41ea0c054f624
irony <cl@iteam.se>
2014-03-23 17:46:07 -0700

    bugfix_ids

--- models/group.js
+++ models/group.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose'),
 
 var GroupSchema = new mongoose.Schema({
       value : { type: {}},
-      userId : { type: Schema.Types.ObjectId },
+      userId : { type: Schema.Types.ObjectId, ref: 'User' },
       photos : { type: [Schema.Types.ObjectId], ref: 'Photo' },
       from : { type: Date },
       to : { type: Date }
