bb410b8a84628e02581c766b1cdf455894b7be72
irony <cl@iteam.se>
2014-04-23 17:19:49 -0700

    worker wait

---
+++ models/cluster.js
@@ -0,0 +1,15 @@
+// Clusters of users collection of groups
+// =====
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var Cluster = new mongoose.Schema({
+  userId : { type: Schema.Types.ObjectId, ref: 'User' },
+  groups : [{}],
+  centroids : [],
+  modified : { type: Date, default: Date.now }
+});
+
+
+module.exports = mongoose.model('cluster', Cluster);
